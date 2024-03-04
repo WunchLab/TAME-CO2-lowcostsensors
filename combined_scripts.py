73c1ea1ae3043382f5bba3505d8afb0f6a20e912
mg-pan <mark.panas@utoronto.ca>
2024-03-04 09:30:56 -0500

    Collect more raw data

--- combined_scripts.py
+++ combined_scripts.py
@@ -12,12 +12,12 @@
 # NB: The modbus_protocol, serial_port, and utilities packages are not publicly available.
 # Contact the repository maintainer for access to those scripts.
 
-import collections
 import csv
-import sys
+import os
+import subprocess
 from datetime import datetime as dt
 from datetime import timedelta
-from time import localtime, sleep, strftime
+from time import sleep
 
 
 #import Adafruit_DHT as DHT
@@ -47,6 +47,7 @@ def get_init_sensor_data(portname):
     # a dictionary with sensor identification data { name: (address, type, size) }
     parameters = utilities.load_parameters_from_file('/home/pi/Desktop/Python3Scripts/hpp_identification.config')
     if not parameters:
+        print("Error with hpp id config file.")
         return
 
     # try to open communication line
@@ -73,6 +74,9 @@ def get_init_sensor_data(portname):
         else:
             result = p + '=' + err + ' '
         sleep(0.001) # without a little pause some timeouts may occur
+        
+        print("HPP device initialized.")
+        
         return(result)
     
     # close communication after all
@@ -132,143 +136,242 @@ def get_parameter_names(filename):
     for i in lines:
         names.append(i.split(' =')[0])
         
+    print("Loaded parameter names.")
+        
     return names
 
 
 
-'''
-This Python script reads the data from the HPP and logs it onto a CSV file.
-It creates two CSV files, one that is 10 second averages and the other is 1 minute averaged
-'''
+# '''
+# This Python script reads the data from the HPP and logs it onto a CSV file.
+# It creates two CSV files, one that is 10 second averages and the other is 1 minute averaged
+# '''
 
 
-'''
-This function returns an array that is organized so that it can be written to the 10 second average CSV file
-'''
-def organize(sample_number, timestamp, numbers):
-    array = [sample_number, timestamp] 
+# '''
+# This function returns an array that is organized so that it can be written to the 10 second average CSV file
+# '''
+# def organize(sample_number, timestamp, numbers):
+#     array = [sample_number, timestamp] 
     
-    for i in range(len(numbers)):
-        data = np.array(numbers[i])
-        array.extend([len(numbers[i]), np.mean(data), np.std(data, ddof=0)]) # Sample count, Mean and standard deviation of data in array
+#     for i in range(len(numbers)):
+#         data = np.array(numbers[i])
+#         array.extend([len(numbers[i]), np.mean(data), np.std(data, ddof=0)]) # Sample count, Mean and standard deviation of data in array
         
-    return array
+#     return array
 
-'''
-This function returns an array that is processed so that it can be written to the 1 minute average CSV file
-'''
-def process(array, length, DHT):
-    new_array = [(dt.now() - timedelta(minutes=1)).strftime('%Y-%m-%d %H:%M:%S')] # Subtract 1 minute from current time for time delay
-    data = np.array([0 for i in range(length * 2)], dtype=float)
+# '''
+# This function returns an array that is processed so that it can be written to the 1 minute average CSV file
+# '''
+# def process(array, length, DHT):
+#     new_array = [(dt.now() - timedelta(minutes=1)).strftime('%Y-%m-%d %H:%M:%S')] # Subtract 1 minute from current time for time delay
+#     data = np.array([0 for i in range(length * 2)], dtype=float)
     
-    for sample in array:
-        for j in range(length * 2):
-            data[j] += sample[j]
+#     for sample in array:
+#         for j in range(length * 2):
+#             data[j] += sample[j]
     
-    data[:2] /= 10 # Divide CO2 concentration and standard deviation by 10
-    data[2:6] /= 100 # Divide pressure, temperature and their standard deviation by 100
-    data /= len(array) # Get average
+#     data[:2] /= 10 # Divide CO2 concentration and standard deviation by 10
+#     data[2:6] /= 100 # Divide pressure, temperature and their standard deviation by 100
+#     data /= len(array) # Get average
     
-    new_array.extend(data)
+#     new_array.extend(data)
     
-    if DHT:
-        new_array.append(absolute_humidity(data[-4], data[-2])) # Returns absolute humidity from DHT's temperature and relative humidity
+#     if DHT:
+#         new_array.append(absolute_humidity(data[-4], data[-2])) # Returns absolute humidity from DHT's temperature and relative humidity
 
-    return new_array
+#     return new_array
     
+def setup_ntp_sync():
+    
+    ## check if system time is sync'd with ntp
+    ntp_status = subprocess.run(["timedatectl show | grep NTPSynchronized"], 
+                                shell=True, capture_output=True, 
+                                text=True).stdout.strip('\n').split('=')[1]
+        
+    if ntp_status == 'yes':
+        print("Time is synchronized")
+        
+    else:
+        print("Time is not synchronized...")
+        #TODO reboot timedatectl once to see if pi will sync
+            
+def setup_paths_filenames(hpp_name):
+    ## check whether the current date's directory and files exist
+    ## if they don't, set them up
+    
+    time_file = dt.now()
+    # TODO check if local or UTC on pi
+    
+    ## set up new year and month directories
+    if os.path.exists(os.path.join("/home/pi/Desktop/Logging_reports", time_file.strftime("%Y"))) == False:
+        
+        subprocess.run(["mkdir " + os.path.join("/home/pi/Desktop/Logging_reports", time_file.strftime("%Y"))], shell=True)
+        print("Created new year directory.")
+        subprocess.run(["mkdir " + os.path.join("/home/pi/Desktop/Logging_reports", time_file.strftime("%Y"), time_file.strftime("%m"))], shell=True)
+        print("Created new month directory.")
+
+    ## set up new month directory
+    elif os.path.exists(os.path.join("/home/pi/Desktop/Logging_reports", time_file.strftime("%Y"), time_file.strftime("%m"))) == False:
+            
+        subprocess.run(["mkdir " + os.path.join("/home/pi/Desktop/Logging_reports", time_file.strftime("%Y"), time_file.strftime("%m"))], shell=True)
+        print("Created new month directory.")
+
+    ## set path and filename variables
+    path = os.path.join("/home/pi/Desktop/Logging_reports", time_file.strftime("%Y"), time_file.strftime("%m"))
+    filename = hpp_name + time_file.strftime("-%Y-%m-%d") + ".csv"
+    
+    ## run the SD card check on mondays
+    if time_file.weekday() == 0:
+        print("Happy Monday!")
+        check_sd_health()
+    
+    return path, filename, time_file
+
+def check_sd_health():
+    ## use smartmontools to check the SD card
+    
+    print("SD card health check...")
+    # TODO add this
+    
+def setup_csv_headers(path, filename, csv_header, csv_col_names):
+    
+    ## check for an existing file, and write header to file if not
+    if os.path.exists(os.path.join(path, filename)) == False:
+        with open(os.path.join(path, filename),'w') as f:
+            wr = csv.writer(f, dialect='excel')
+            wr.writerow(csv_header)
+            wr.writerow(["#"])
+            wr.writerow(csv_col_names)
+        print("New file written.\n\tPath = %s\n\tFilename = %s" % (path, filename))
+
+def load_location_config(file_path):
+    
+    location_config =[]
+    
+    with open(file_path, 'r') as f:
+        raw = f.readlines()
+    
+    for i in range(len(raw)):
+        print(raw[i].strip('\n'))
+        location_config.extend(raw[i].strip('\n').replace(' ', '').split('='))
+    
+    return location_config
+
+def make_parameter_formats(csv_col_names):
+    
+    ## set up formats for the csv for particular measured parameters
+    ## set up as a dictionary where the keys are the index in the row
+    ## and the values are a list containing information in index 0 and
+    ## the fstrings format code in index 1
+    
+    parameter_formats = {}
+    
+    for i in range(len(csv_col_names)):
+        
+        if "Error" in csv_col_names[i]:
+            parameter_formats[i] = ['hex',"08b"]
+            
+    return parameter_formats
+
 def start_logging(interval):
+    ## This is the main sensor function
+    ## Initializes the sensor and then takes measurements every interval seconds
+    
+    ## set paths for important script parts
     port = '/dev/ttyUSB0' # Port for Raspberry Pi 3
-    config = '/home/pi/Desktop/Python3Scripts/my_parameters.config' # Path where to read which parameters to log
+    config = '/home/pi/Desktop/Python3Scripts/my_parameters_TEST.config' # Path where to read which parameters to log
 
-    csv_header = [['', ''], ['Sample #', 'Timestamp']] # Headers for CSV file
-    csv_header_processed = ['Timestamp']
-    
+    hpp_serial = get_init_sensor_data(port).strip(' ')
+    hpp_name = "HPP"+hpp_serial[-4:] # Last 4 digits of meter ID of HPP
+
+    csv_header = ["#","HPP_serial", hpp_serial]
+    location_config = load_location_config("/home/pi/Desktop/Python3Scripts/location.config")
+    csv_header.extend(location_config)
+
+    ## get parameter names and set up csv header
+    csv_col_names = ['Timestamp']
     parameters = get_parameter_names(config)
-    parameter_length = len(parameters) + 2
+    csv_col_names.extend(parameters)
+    csv_col_names.extend(["DHT_Temperature", "DHT_Relative_Humidity", "CPU_Temperature"])
+    parameter_formats = make_parameter_formats(csv_col_names)
     
-    for name in parameters:
-        csv_header[0].extend(['', name, ''])
-        csv_header[1].extend(['Sample count', 'Average', 'StdDev'])
-        
-        csv_header_processed.extend([name, 'StdDev'])
-        
-    csv_header[1].extend(['Sample count', 'Average', 'StdDev', 'Sample count', 'Average', 'StdDev'])
-    csv_header[0].extend(['', 'DHT_Temperature', '', '', 'DHT_Relative_Humidity', ''])
-    csv_header_processed.extend(['DHT_Temperature', 'StdDev', 'DHT_Relative_Humidity', 'StdDev', 'Calculated Absolute Humidity'])
+    ## run important initialization steps
+    ## see individual functions for more info
+    setup_ntp_sync()
+    check_sd_health()
+    path, filename, time_file = setup_paths_filenames(hpp_name)
+    setup_csv_headers(path, filename, csv_header, csv_col_names)
     
-    hpp_name = get_init_sensor_data(port)[-5:] # Meter ID of HPP
-    current_time = dt.now().strftime('_%Y%m%d_%H%M%S')
-    
-    path  = '/home/pi/Desktop/Logging_reports/HPP_' + hpp_name + current_time + '.csv' # create path for CSV files
-    path_processed = '/home/pi/Desktop/Processed/HPP_' + hpp_name + current_time + '_processed_PERC5.csv'     
-
-    file = open(path, 'w') # Write the headers to the CSV files
-    writer = csv.writer(file)
-    writer.writerows(csv_header)
-
-    file_processed = open(path_processed, 'w')
-    writer_processed = csv.writer(file_processed)
-    writer_processed.writerows([csv_header_processed])
-    
-    sample_number = 1
-    
-    start_minute = dt.now().strftime('%M')
-    processed_array = []
-    
+    ## initialize variables for the measurement loop
     DHT_works = True #Boolean to control if DHT works or not
+    time_prev = False
     
-    while True: # Never ending loop
-        numbers = [[] for i in range(parameter_length)]
-        start_second = dt.now().strftime('%S')[0]
+    print("Beginning measurement loop...")
+    
+    while True: # Never ending loop for measurement
+    
+        row = []
+        time0 = dt.now()
         
-        while True:
-            raw_data = get_logg_sensor_data(port, config) # Recieve data from HPP sensor
-            if DHT_works:
-                raw_DHT = DHT22.DHT_sensor(9) # Recieve temperature and relative humidity from DHT
-                if str(raw_DHT[0]) == 'None': #If DHT returns None then it has stopped working
-                    DHT_works = False
-                    parameter_length -= 2
-                    numbers = [[] for i in range(parameter_length)]
-                else:
-                    raw_data += raw_DHT
-       
-            for i in range(parameter_length):
-                numbers[i].append(raw_data[i])                
-                
-            for i in range(int(interval/0.1)): # Break down interval into multiple pieces to read if the time has changed in between
-                if start_second == dt.now().strftime('%S')[0]:
-                    sleep(0.1)
-                else:
-                    break
-                
+        row.extend(get_logg_sensor_data(port, config)) # Recieve data from HPP sensor
+        
+        if DHT_works:
+            
+            raw_DHT = DHT22.DHT_sensor(9) # Recieve temperature and relative humidity from DHT
+            if str(raw_DHT[0]) == 'None': #If DHT returns None then it has stopped working
+                DHT_works = False
+                row.extend([-9999,-9999])
             else:
-                continue
-            break # If the time by 10 seconds has changed during sleep then code goes out of inner while loop
+                row.append(int(round(raw_DHT[0],1)*10))
+                row.append(int(round(raw_DHT[1],1)*10))
                 
-        data_array = organize(sample_number, dt.now().strftime('%Y-%m-%d %H:%M:%S'), numbers)
-          
-        file = open(path, 'a') # Write to 10 second average CSV file
-        writer = csv.writer(file)
-        writer.writerows([data_array])
-        sample_number += 1
+        else:
+            row.extend([-9999,-9999])
+   
+        ## read in the CPU temperature
+        row.append(round(int(subprocess.run(["cat /sys/class/thermal/thermal_zone0/temp"], # TODO check operation on pi
+                       shell=True, capture_output=True, text=True
+                       ).stdout.strip("\n")),-2) // 100)
+            
+        time1 = dt.now()
         
-        if start_minute != dt.now().strftime('%M'): # If the minute has changed write to 1 minute CSV file
-            if len(processed_array) != 0:
-                file_processed = open(path_processed, 'a')
-                writer_processed = csv.writer(file_processed)
-                writer_processed.writerows([process(processed_array, parameter_length, DHT_works)])
+        time_row = time0 + (time1 - time0)/2
+        
+        row.insert(0, time_row.isoformat(timespec='milliseconds'))
+        
+        ## adjust formats for certain parameters in row
+        for k,v in parameter_formats.items():
+            
+            if v[0] == "hex":
+                row[k] = f"{int(row[k],0):{v[1]}}"
+            else:
+                row[k] = f"{row[k]:{v[1]}}"
+        
+        ## check if the day changed during the measurement
+        if time_row.day != time_file.day:
+            setup_ntp_sync()
+            path, filename, time_file = setup_paths_filenames(hpp_name)
+                
+        ## check for an existing file, and write header to file if not
+        setup_csv_headers(path, filename, csv_header, csv_col_names)
+        
+        ## append measured values to csv
+        with open(os.path.join(path, filename),'a') as f:
+            wr = csv.writer(f, dialect='excel')
+            wr.writerow(row)
 
-                processed_array = []
-            
-            start_minute = dt.now().strftime('%M')
+        ## control the timing of the measurement loop based on the interval
+        if time_prev:
+            time_now = dt.now()
+            if (time_now - time_prev).total_seconds() < interval:
+                try:
+                    sleep(interval - (time_now - time_prev).total_seconds())
+                except:
+                    pass
+        else:
+            sleep(interval)
+        time_prev = time_row
         
-        val = True # If the standard deviation is greater than 10, don't log the data 
-        for std in data_array[4::3]:
-            if std > 10:
-                val = False
-                break
-            
-        if val:
-            processed_array.append([num for index, num in enumerate(data_array[2:]) if index % 3 != 0]) # Only process data and standard deviation not sample count
-            
-start_logging(2)
+if __name__ == "__main__":
+    start_logging(2.5)
