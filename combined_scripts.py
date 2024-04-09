654b615632a003f730c57983d96f2882e557ec7d
mg-pan <mark.panas@utoronto.ca>
2024-04-09 14:20:50 -0400

    Added GPS functionality

--- combined_scripts.py
+++ combined_scripts.py
@@ -18,6 +18,9 @@ import subprocess
 from datetime import datetime as dt
 from datetime import timedelta
 from time import sleep
+import gps
+import glob
+import sys
 
 
 #import Adafruit_DHT as DHT
@@ -119,7 +122,12 @@ def get_logg_sensor_data(portname, config):
                 5 + size)
         if ok:
             #result += p + '=' + utilities.parse_parameter(size, type, data) + ' '
-            result.append(utilities.parse_parameter(size, type, data))
+            try:
+                result.append(utilities.parse_parameter(size, type, data))
+            except Exception as e:
+                print("Exception: %s" % e)
+                print("Error occurred parsing: %s" % str(parameters[p]))
+                result.append(-9999)
         else:
             result += p + '=' + err + ' '
         sleep(0.001)
@@ -181,24 +189,108 @@ def get_parameter_names(filename):
 #         new_array.append(absolute_humidity(data[-4], data[-2])) # Returns absolute humidity from DHT's temperature and relative humidity
 
 #     return new_array
+
+def poll_gps(gps_session, timeout):
     
-def setup_ntp_sync():
+    ## This function reads the gps data via the gpsd client
+    ## The code is drawn from:
+    ## https://gpsd.io/gpsd-client-example-code.html#_example_2python
+    
+    gps_dict = {
+            "fix" : "Invalid",
+            "lat" : -9999,
+            "lon" : -9999,
+            "alt" : -9999
+    }
+    
+    poll_start = dt.now()
+    gps_read_try_n = 0
+    
+    while True:
+                
+        ## check for time out
+        if (dt.now() - poll_start) > timedelta(seconds = timeout):
+            print("GPS signal acquisition timed out (%d s)." % timeout)
+            gps_session.close()
+            gps_session = gps.gps(mode=gps.WATCH_ENABLE)
+            print("GPS session was restarted.")
+            break
+        
+        try:
+            ## check the read is OK. The read returns negative if not OK.
+            if gps_session.read() != 0:
+                gps_read_try_n += 1
+                if gps_read_try_n >= 2:
+                    print("GPS session read failed multiple times.")
+                    gps_session.close()
+                    gps_session = gps.gps(mode=gps.WATCH_ENABLE)
+                    print("GPS session was restarted.")
+                    gps_read_try_n = 0
+                sleep(1)
+                continue
+            
+            ## check that the packet contains time, position, velocity (TPV) data
+            if not (gps.MODE_SET & gps_session.valid):
+                gps_read_try_n += 1
+                if gps_read_try_n >= 2:
+                    print("GPS session read packet was invalid multiple times.")
+                    gps_session.close()
+                    gps_session = gps.gps(mode=gps.WATCH_ENABLE)
+                    print("GPS session was restarted.")
+                    gps_read_try_n = 0
+                sleep(1)
+                continue
+                
+            ## check the GPS fix. 2 is 2D fix and 3 is 3D fix
+            if gps_session.fix.mode >= 2 and gps.isfinite(gps_session.fix.latitude):
+                
+                gps_dict["fix"] = ("Invalid", "NO_FIX", "2D", "3D")[gps_session.fix.mode]
+                gps_dict["lat"] = gps_session.fix.latitude
+                gps_dict["lon"] = gps_session.fix.longitude
+                gps_dict["alt"] = gps_session.fix.altitude
+                
+                print("GPS data acquired:\n%s" % gps_dict)
+                
+                break
+            
+        except ConnectionResetError as e:
+            print("GPS error: "+str(e))
+            gps_session.close()
+            gps_session = gps.gps(mode=gps.WATCH_ENABLE)
+            print("GPS session was restarted.")
+            break
+        
+    return gps_dict, gps_session
+    
+def check_time_sync(printing=True):
     
     ## check if system time is sync'd with ntp
-    ntp_status = subprocess.run(["timedatectl show | grep NTPSynchronized"], 
+    chrony_last_sync = subprocess.run(["chronyc sources | grep NMEA"], 
                                 shell=True, capture_output=True, 
-                                text=True).stdout.strip('\n').split('=')[1]
-        
-    if ntp_status == 'yes':
-        print("Time is synchronized")
-        
-    else:
-        print("Time is not synchronized...")
-        #TODO reboot timedatectl once to see if pi will sync
+                                text=True).stdout.split()[5]
+    
+    chrony_offset = subprocess.run(["chronyc sources | grep NMEA"], 
+                                shell=True, capture_output=True, 
+                                text=True).stdout.split()[6].split('[')[0]
+
+    if printing:
+        try:
+            if int(chrony_last_sync) <= 13:
+                print("Time is synchronized")
+                
+            else:
+                print("Time is not synchronized...")
+                
+        except:
+            print("Time is not synchronized...")
+    
+    return chrony_last_sync, chrony_offset
             
 def setup_paths_filenames(hpp_name):
     ## check whether the current date's directory and files exist
     ## if they don't, set them up
+    
+    daily_file_counter = 0
     
     time_file = dt.now()
     # TODO check if local or UTC on pi
@@ -219,20 +311,20 @@ def setup_paths_filenames(hpp_name):
 
     ## set path and filename variables
     path = os.path.join("/home/pi/Desktop/Logging_reports", time_file.strftime("%Y"), time_file.strftime("%m"))
-    filename = hpp_name + time_file.strftime("-%Y-%m-%d") + ".csv"
+    filename = hpp_name + time_file.strftime("-%Y-%m-%d") + "--" + str(daily_file_counter) +".csv"
+    
+    ## tick up the end of the filename if a file for the day already exists
+    if os.path.exists(os.path.join(path, filename)) == True:
+        daily_files_list = glob.glob(os.path.join(path, filename.split('--')[0]+'*.csv'))
+        daily_file_counter = len(daily_files_list)
+        filename = hpp_name + time_file.strftime("-%Y-%m-%d") + "--" + str(daily_file_counter) +".csv"
     
     ## run the SD card check on mondays
     if time_file.weekday() == 0:
         print("Happy Monday!")
-        check_sd_health()
+        #check_sd_health()
     
     return path, filename, time_file
-
-def check_sd_health():
-    ## use smartmontools to check the SD card
-    
-    print("SD card health check...")
-    # TODO add this
     
 def setup_csv_headers(path, filename, csv_header, csv_col_names):
     
@@ -279,17 +371,33 @@ def start_logging(interval):
     ## Initializes the sensor and then takes measurements every interval seconds
     
     ## set paths for important script parts
-    port = '/dev/ttyUSB0' # Port for Raspberry Pi 3
-    config = '/home/pi/Desktop/Python3Scripts/my_parameters_TEST.config' # Path where to read which parameters to log
+    port = subprocess.run(["ls /dev/serial/by-id/usb-FTDI*"], # TODO check operation on pi
+                   shell=True, capture_output=True, text=True
+                   ).stdout.strip("\n")
+    
+    # port = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A600PJOV-if00-port0' # Port for Raspberry Pi 3
+    config = '/home/pi/Desktop/Python3Scripts/my_parameters.config' # Path where to read which parameters to log
 
     hpp_serial = get_init_sensor_data(port).strip(' ')
     hpp_name = "HPP"+hpp_serial[-4:] # Last 4 digits of meter ID of HPP
 
+    ## set up GPS and record location
+    gps_session = gps.gps(mode=gps.WATCH_ENABLE)
+    gps_dict, gps_session = poll_gps(gps_session, 10)   # 10 s time out
+
+    ## set up header and read information from location config file
     csv_header = ["#","HPP_serial", hpp_serial]
     location_config = load_location_config("/home/pi/Desktop/Python3Scripts/location.config")
+    
+    ## prefer GPS lat lon over config file lat lon
+    if gps_dict["fix"] == '2D' or gps_dict["fix"] == '3D':
+        location_config[5] = gps_dict["lat"]
+        location_config[7] = gps_dict["lon"]
+        location_config[9] = gps_dict["alt"]
+    
     csv_header.extend(location_config)
 
-    ## get parameter names and set up csv header
+    ## get parameter names and set up csv column names header
     csv_col_names = ['Timestamp']
     parameters = get_parameter_names(config)
     csv_col_names.extend(parameters)
@@ -298,14 +406,14 @@ def start_logging(interval):
     
     ## run important initialization steps
     ## see individual functions for more info
-    setup_ntp_sync()
-    check_sd_health()
+    chrony_last_sync, chrony_offset = check_time_sync()
     path, filename, time_file = setup_paths_filenames(hpp_name)
     setup_csv_headers(path, filename, csv_header, csv_col_names)
     
     ## initialize variables for the measurement loop
     DHT_works = True #Boolean to control if DHT works or not
     time_prev = False
+    counter = 0
     
     print("Beginning measurement loop...")
     
@@ -313,6 +421,25 @@ def start_logging(interval):
     
         row = []
         time0 = dt.now()
+        
+        if counter == 100:
+            
+            gps_dict, gps_session = poll_gps(gps_session, 10)
+            chrony_last_sync, chrony_offset = check_time_sync(printing=False)
+            
+            with open("/home/pi/Desktop/Logging_reports/GPS_log.txt", 'a') as f:
+                f.write(time0.isoformat(timespec='milliseconds'))
+                for v in gps_dict.values():
+                    f.write(' '+str(v))
+                f.write('\n')
+                
+            with open("/home/pi/Desktop/Logging_reports/chrony_log.txt", 'a') as f:
+                f.write(time0.isoformat(timespec='milliseconds'))
+                f.write(' '+str(chrony_last_sync))
+                f.write(' '+str(chrony_offset))
+                f.write('\n')
+            
+            counter = 0
         
         row.extend(get_logg_sensor_data(port, config)) # Recieve data from HPP sensor
         
@@ -344,13 +471,18 @@ def start_logging(interval):
         for k,v in parameter_formats.items():
             
             if v[0] == "hex":
-                row[k] = f"{int(row[k],0):{v[1]}}"
+                try:
+                    row[k] = f"{int(row[k],0):{v[1]}}"
+                except Exception as e:
+                    print("Parameter exception: %s" % e)
+                    print(row[k])
+                    row[k]=-9999
             else:
                 row[k] = f"{row[k]:{v[1]}}"
         
         ## check if the day changed during the measurement
         if time_row.day != time_file.day:
-            setup_ntp_sync()
+            chrony_last_sync, chrony_offset = check_time_sync()
             path, filename, time_file = setup_paths_filenames(hpp_name)
                 
         ## check for an existing file, and write header to file if not
@@ -372,6 +504,15 @@ def start_logging(interval):
         else:
             sleep(interval)
         time_prev = time_row
+        counter += 1
         
 if __name__ == "__main__":
+    
+    try:
+        initial_sleep = int(sys.argv[1])
+    except IndexError:
+        initial_sleep = 60
+        
+    sleep(initial_sleep)   # initial sleep to allow for user interrupt and gps cold start
+    
     start_logging(2.5)
